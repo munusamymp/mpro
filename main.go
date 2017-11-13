50bbbff34885c724f3db35726b40a72b28025b3c
munusamy <munuswamymp@gmail.com>
2017-11-12 16:57:06 -0800

    Added Dummy query features

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
-import "fmt"
+
+import (
+        "fmt"
+        "os"
+        "strings"
+
+        flag "github.com/ogier/pflag"
+)
+
+// flags
+var user string
+
 func main() {
-    fmt.Println("My First Go Program")
+        // parse flags
+        flag.Parse()
+
+        // if user does not supply flags, print usage
+        // we can clean this up later by putting this into its own function
+        if flag.NFlag() == 0 {
+                fmt.Printf("Usage: %s [options]\n", os.Args[0])
+                fmt.Println("Options:")
+                flag.PrintDefaults()
+                os.Exit(1)
+        }
+        users := strings.Split(user, ",")
+        fmt.Printf("Searching user(s): %s\n", users)
+}
+
+func init() {
+        flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
